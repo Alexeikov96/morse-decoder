daccca197ac83fb71f2a4089d2d5e3e99ed6a6be
alexeikov96 <speedfaer96@gmail.com>
2023-02-11 23:17:11 +0300

    feat: added a solution to this task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    
+    let decodeString = '';    
+
+    decoder:
+    for (let i = 0; i < expr.length; i += 10) {
+
+        let newExpr = expr.slice(i, i + 10);
+        let codeString = '';
+        
+        for (let j = 0; j < newExpr.length; j += 2) {
+            if (newExpr === '**********') {
+                decodeString += ' ';
+                continue decoder;
+            } else if (newExpr.slice(j, j + 2) === '10') {
+                codeString += '.';
+            } else if (newExpr.slice(j, j + 2) === '11') {
+                codeString += '-';
+            } else if (newExpr.slice(j, j + 2) === '00') {
+                codeString += '';
+            }
+            
+        }
+
+        decodeString += MORSE_TABLE[codeString];
+    }
+
+    return decodeString;
 }
 
 module.exports = {
     decode
 }
+
+
